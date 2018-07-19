19a046320930f520f093c6be5ba264d2bd070fd3
rkim91 <richeekim@gmail.com>
2018-07-19 13:57:24 -0400

    can render summoner details

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import Searchbar from "./components/Searchbar";
+import SummonerDetails from "./components/SummonerDetails";
+import SummonerProfileCard from "./components/SummonerProfileCard";
 import { Header } from "semantic-ui-react";
 
 const summonerNameURL = "http://localhost:3000/summoner_name";
+const summonerDataURL = "http://localhost:3000/summoner_id_data";
 
 class App extends Component {
   constructor() {
@@ -13,6 +16,13 @@ class App extends Component {
       summonerName: "",
       accountId: "",
       summonerId: "",
+      summonerQueue: "",
+      summonerLeagueName: "",
+      summonerTier: "",
+      summonerRank: "",
+      summonerLP: "",
+      summonerWins: "",
+      summonerLosses: "",
       matchList: [],
       championList: []
     };
@@ -22,37 +32,67 @@ class App extends Component {
     let searchingTerm = e.target.value;
 
     this.setState({
-      searchTerm: searchingTerm
+      searchTerm: searchingTerm.split(" ").join("_")
     });
   };
 
   componentDidMount() {
-    // this.getSummonerData();
+    // this.getSummonerDetails();
+    // this.searchSummonerName();
   }
 
+  searchSummonerName = () => {
+    if (this.state.searchTerm) {
+      return fetch(summonerNameURL + `/${this.state.searchTerm}`)
+        .then((res) => res.json())
+        .then((summoner) =>
+          this.setState(
+            {
+              summonerName: summoner.name,
+              accountId: summoner.accountId,
+              summonerId: summoner.id
+            },
+            () => {
+              this.getSummonerData();
+            }
+            // Promise.resolve(true)
+          )
+        );
+    } else {
+      // return Promise.resolve(false);
+    }
+  };
+
   getSummonerData = () => {
-    fetch(summonerNameURL)
+    fetch(summonerDataURL + `/${this.state.summonerId}`)
       .then((res) => res.json())
       .then((summoner) =>
         this.setState({
-          summonerName: summoner.name,
-          accountId: summoner.accountId,
-          summonerId: summoner.id
+          summonerQueue: this.filterQueueType(summoner)[0].queueType,
+          summonerLeagueName: this.filterQueueType(summoner)[0].leagueName,
+          summonerTier: this.filterQueueType(summoner)[0].tier,
+          summonerRank: this.filterQueueType(summoner)[0].rank,
+          summonerLP: this.filterQueueType(summoner)[0].leaguePoints,
+          summonerWins: this.filterQueueType(summoner)[0].wins,
+          summonerLosses: this.filterQueueType(summoner)[0].losses
         })
       );
   };
 
-  searchSummonerName = () => {
-    fetch(summonerNameURL + `/${this.state.searchTerm}`)
-      .then((res) => res.json())
-      .then((summoner) =>
-        this.setState({
-          summonerName: summoner.name,
-          accountId: summoner.accountId,
-          summonerId: summoner.id
-        })
-      );
+  filterQueueType = (summoner) => {
+    // console.log(summoner);
+    return summoner.filter(
+      (summoner) => summoner.queueType === "RANKED_SOLO_5x5"
+    );
   };
+
+  // getSummonerMatches = () => {
+  //   fetch;
+  // };
+
+  // getSummonerDetails = () => {
+  //   this.searchSummonerName().then(this.getSummonerData());
+  // };
 
   // searchTransactions = () => {
   //   if (this.state.searchTerm) {
@@ -77,9 +117,35 @@ class App extends Component {
           term={this.state.searchTerm}
           findSummoner={this.searchSummonerName}
         />
+
+        {this.state.summonerTier ? (
+          <SummonerProfileCard
+            summonerName={this.state.summonerName}
+            accountId={this.state.accountId}
+            summonerQueue={this.state.summonerQueue}
+            summonerLeagueName={this.state.summonerLeagueName}
+            summonerTier={this.state.summonerTier}
+            summonerRank={this.state.summonerRank}
+            summonerLP={this.state.summonerLP}
+            summonerWins={this.state.summonerWins}
+            summonerLosses={this.state.summonerLosses}
+          />
+        ) : null}
       </div>
     );
   }
 }
+
+// <SummonerDetails
+//   summonerName={this.state.summonerName}
+//   accountId={this.state.accountId}
+//   summonerQueue={this.state.summonerQueue}
+//   summonerLeagueName={this.state.summonerLeagueName}
+//   summonerTier={this.state.summonerTier}
+//   summonerRank={this.state.summonerRank}
+//   summonerLP={this.state.summonerLP}
+//   summonerWins={this.state.summonerWins}
+//   summonerLosses={this.state.summonerLosses}
+// />
 
 export default App;
---
+++ src/components/SummonerDetails.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class SummonerDetails extends React.Component {
+  render() {
+    return <h1>{this.props.summonerName}</h1>;
+  }
+}
+
+export default SummonerDetails;
